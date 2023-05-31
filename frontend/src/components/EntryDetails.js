b52e1bf014da56f1420bea089062dda9f02f3af0
Roger Earl <rogerearl@me.com>
2023-05-30 21:40:27 -0700

    Added Google font to entries.

--- frontend/src/components/EntryDetails.js
+++ frontend/src/components/EntryDetails.js
@@ -4,8 +4,8 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 export default function EntryDetails({ entry }) {
     return (
         <div className="bg-gray-200 max-w-sm rounded overflow-hidden shadow-lg p-2.5">
-            <h4 className="font-bold text-xl mb-2">{entry.title}</h4>
-            <p>{entry.message}</p>
+            <h4 className="font-bold text-xl mb-2 writing">{entry.title}</h4>
+            <p className="writing">{entry.message}</p>
             <p className="text-xs italic">{formatDistanceToNow(new Date(entry.createdAt), { addSuffix: true })}</p>
         </div>
     )
