6c0fec02e47d72aa8004305bcc1d325a4d2bc7e8
Roger Earl <rogerearl@me.com>
2023-06-03 10:58:03 -0700

    Use JWT to authenticate user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d' })
+}
 
 // login user
 async function loginUser(req, res) {
-    res.json({message: 'login user'})
+    const { email, password } = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 // signup user
@@ -12,7 +28,10 @@ async function signupUser(req, res) {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,4 +37,26 @@ userSchema.statics.signup = async function(email, password) {
     return user
 }
 
+userSchema.statics.login = async function(email, password) {
+    if (!email || !password) {
+        throw Error('All fields must be filled.')
+    }
+
+    const user = await this.findOne({ email })
+
+    if (!user) {
+        throw Error('Please enter the correct email.')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) {
+        throw Error('Password is incorrect.')
+    }
+
+    return user
+}
+
+
+
 module.exports = mongoose.model('User', userSchema)
