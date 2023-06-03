de9c4bede18bb9f546f966305dff331c3f0cfcb5
Roger Earl <rogerearl@me.com>
2023-06-03 13:10:43 -0700

    Added authentication context.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { EntriesContextProvider } from './context/EntryContext';
+import { AuthContext, AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <EntriesContextProvider>
-      <App />
-    </EntriesContextProvider>
+    <AuthContextProvider>
+      <EntriesContextProvider>
+        <App />
+      </EntriesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
