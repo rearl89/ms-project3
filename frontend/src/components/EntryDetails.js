9bc0458be2d70f2165b13edd1ef86e5e8ab0d081
Roger Earl <rogerearl@me.com>
2023-06-05 19:54:06 -0700

    Entries and entry form sizing to fit better.

--- frontend/src/components/EntryDetails.js
+++ frontend/src/components/EntryDetails.js
@@ -21,7 +21,7 @@ export default function EntryDetails({ entry }) {
     }
 
     return (
-        <div className="bg-gray-200 max-w-fit rounded overflow-hidden shadow-lg p-2.5">
+        <div className="entries bg-gray-200 max-w-fit rounded overflow-hidden shadow-lg p-2.5">
             <h4 className="font-bold text-xl mb-2 writing">{entry.title}</h4>
             <p className="writing">{entry.message}</p>
             <p className="text-xs italic">{formatDistanceToNow(new Date(entry.createdAt), { addSuffix: true })}</p>
