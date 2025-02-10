289981e26c190e859d0faeebbb536aee50831d54
Roger Earl <rogerearl@me.com>
2025-02-09 22:01:48 -0800

    Updated server.js file.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const entryRoutes = require('./routes/entries')
-const userRoutes = require('./routes/user')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const entryRoutes = require("./routes/entries");
+const userRoutes = require("./routes/user");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/entries', entryRoutes)
-app.use('/user', userRoutes)
+app.use("/entries", entryRoutes);
+app.use("/user", userRoutes);
 
 // connects to database
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
-        console.log('listening on port 5001')
-    }) 
-})
-.catch((error) => {
-    console.log(error)
-})
-
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
